19f5db7e1be3a553b6074424c0425b9514bd1ad8
Juan Barrera <jcbp1999@gmail.com>
2024-08-24 01:09:16 +1000

    Initial commit with postgreSQL local URI configured

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    TESTING = True
+    
+class LocalDevelopmentConfig(Config):
+    SECRET_KEY = os.getenv("LOCAL_DEV_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("LOCAL_DEV_DB_URI")
