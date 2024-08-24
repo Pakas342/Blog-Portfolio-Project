6540e6060d2cc0da24727fb47f5a48e672cbd697
Juan Barrera <jcbp1999@gmail.com>
2024-08-24 20:23:22 +1000

    Models added to the db schema, and schema migration completed

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 # Initialize an open instance of SQLAlchemy and Migrate
 
 db = SQLAlchemy()
+# SQLAlchemy was having some issues to find the models, so I forced by importing app.models
+import app.models
 migrate = Migrate(render_as_batch=True)
 
 VALID_CONFIG_CLASSES = {
@@ -15,23 +18,23 @@ VALID_CONFIG_CLASSES = {
 def create_app(config_class_name: str) -> Flask:
     if config_class_name not in VALID_CONFIG_CLASSES:
         raise ValueError(f"The config name {config_class_name} isn't part of the valid config classes")
-    
+
     config_class = VALID_CONFIG_CLASSES[config_class_name]
-    
+
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     # Linking the initialized instance of db and migrate to the app and db context
     db.init_app(app)
     migrate.init_app(app, db)
-    
+
     # Linking a bp to the app
     from app.main_blueprint import bp as main_bp
     app.register_blueprint(main_bp)
     
-    print("Registered routes:")
-    for rule in app.url_map.iter_rules():
-        print(f"{rule.endpoint}: {rule.rule}")
-    
+    # Code for printing the current routes
+    # print("Registered routes:")
+    # for rule in app.url_map.iter_rules():
+    #     print(f"{rule.endpoint}: {rule.rule}")
+
     return app
-    
--- app/models.py
+++ app/models.py
@@ -1,1 +1,36 @@
-# TODO Set the DB schema and the services related to it for the routes to work
+from app import db
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text, ForeignKey, DateTime
+from typing import List
+from datetime import datetime
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    full_name: Mapped[str] = mapped_column(String(1000), nullable=False)
+    email: Mapped[str] = mapped_column(String(1000), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(1000), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+    blog_posts: Mapped[List["BlogPost"]] = relationship(back_populates="author")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="author")
+    
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(1000), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="blog_posts")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="blog")
+    
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    body: Mapped[str] = mapped_column(String(1000), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+    blog_id: Mapped[int] = mapped_column(ForeignKey("blog_post.id"))
+    blog: Mapped["BlogPost"] =  relationship(back_populates="comments")
+    author_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    author: Mapped["User"] = relationship(back_populates="comments")
