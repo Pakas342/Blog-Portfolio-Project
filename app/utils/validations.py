6ad253597e91be4807048b7ba41ef49bf42d6fb7
Juan Barrera <jcbp1999@gmail.com>
2024-09-01 19:55:26 +1000

    Fixed type hinting on validation utils

--- app/utils/validations.py
+++ app/utils/validations.py
@@ -7,17 +7,17 @@ from .functions import create_http_response
 class UserInputsValidation:
 
     @staticmethod
-    def email_validation(email: str) -> Union[None, dict]:
+    def email_validation(email: str) -> Union[None, tuple]:
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
             return create_http_response('Invalid email format', 'failed', 400)
 
     @staticmethod
-    def password_validation(password: str) -> Union[None, dict]:
+    def password_validation(password: str) -> Union[None, tuple]:
         if len(password) < 8:
             return create_http_response('Password must be at least 8 characters long', 'failed', 400)
 
     @staticmethod
-    def validate_existence(*args: Union[str, int]) -> Union[None, dict]:
+    def validate_existence(*args: Union[str, int]) -> Union[None, tuple]:
         for user_input in args:
             if not user_input:
                 return create_http_response(f'{user_input} is required', 'failed', 400)
