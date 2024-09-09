e87131408dc75517262c4bc36f5cb4ad0f074f8c
Juan Barrera <jcbp1999@gmail.com>
2024-09-09 13:46:15 +1000

    Added the post and put handlers

--- app/blueprints/blog.py
+++ app/blueprints/blog.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from ..services.auth import authentication_required
 from ..utils.decorators import require_json
 from ..services.blog import get_blog, get_all_blogs
@@ -6,15 +6,17 @@ from ..services.blog import get_blog, get_all_blogs
 blog_blueprint = Blueprint('blog_blueprint ', __name__)
 
 
-@blog_blueprint.route("/blog", methods=['GET'])
+@blog_blueprint.route("/blog", methods=['GET', 'POST'])
 @require_json(methods=['POST'])
 def get_blogs():
+    if request.method == 'POST':
+        pass
     return get_all_blogs()
 
 
 @blog_blueprint.route("/blog/<int:blog_id>", methods=['GET', 'PUT', 'DELETE'])
 @require_json(methods=['PUT'])
 def get_blog_data(blog_id: int):
+    if request.method == 'PUT':
+        pass
     return get_blog(blog_id)
-
-
